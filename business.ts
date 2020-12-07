c7a4bcd1b7614904166fb8d920539172ceb8975f
Daniel Lima <danielfbl@gmail.com>
2020-12-07 13:26:34 -0300

    added type: restaurant to interface Business

--- business.ts
+++ business.ts
@@ -3,6 +3,7 @@ import { UserProfile } from "./profile";
 export interface Business {
   name: string;
   status: 'open' | 'closed';
+  type: 'restaurant';
 }
 
 export interface BusinessManagerProfile extends UserProfile {
