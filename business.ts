b7af9782ca55f8cc7c78941cf85ce0d0117f52ae
Daniel Lima <danielfbl@gmail.com>
2020-12-15 11:45:44 -0300

    added new types for the Business interface

--- business.ts
+++ business.ts
@@ -1,5 +1,15 @@
 export interface Business {
   name: string;
+  description: string;
+  minimumOrder: number;
+  businessAddress: {
+    address: string;
+    number: string;
+    cep: number;
+    city: string;
+    state: string;
+  }
+  cnpj: number;
   status: 'open' | 'closed';
   type: 'restaurant';
 }
