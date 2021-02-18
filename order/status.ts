5cba7007efcb60755bcaa556f61debdcd4c7ef9b
Pedro Andrade <pdandradeb@gmail.com>
2021-02-18 11:09:45 -0300

    Adding 'dispatching' state back

--- order/status.ts
+++ order/status.ts
@@ -2,6 +2,7 @@ export type P2POrderStatus =
   | 'quote'
   | 'confirming'
   | 'confirmed'
+  | 'dispatching'
   | 'delivered'
   | 'canceled';
 
@@ -11,6 +12,7 @@ export type FoodOrderStatus =
   | 'confirmed'
   | 'preparing'
   | 'ready'
+  | 'dispatching'
   | 'delivered'
   | 'canceled';
 
