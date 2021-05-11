88f7c534c397da2d6ec5123034396ff01950366a
RenanC <renancostam@gmail.com>
2021-05-11 16:09:53 -0300

    Add type 'courier-manager' in Role

--- managers.ts
+++ managers.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import { UserProfile } from './profile';
-export type Role = 'owner' | 'staff' | 'viewer';
+export type Role = 'owner' | 'staff' | 'viewer' | 'courier-manager';
 
 export interface ManagerProfile extends UserProfile {}
 
