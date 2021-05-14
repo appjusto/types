c3b64c52ed18ed83a5a0bedf6c7c70d5714ea0c9
RenanC <renancostam@gmail.com>
2021-05-14 16:14:26 -0300

    Add business companyName

--- business.ts
+++ business.ts
@@ -43,6 +43,7 @@ export interface Business {
   type: BusinessType;
   code?: string;
   name?: string;
+  companyName?: string;
   cnpj?: string;
   phone?: string;
   managers?: string[]; // emails
