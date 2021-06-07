bbac0922745c4733ce26e55c6f0d10d71f1a0896
Pedro Andrade <pdandradeb@gmail.com>
2021-06-07 13:00:33 -0300

    Adding totalDeliveries to PlatformStatistics

--- platform.ts
+++ platform.ts
@@ -45,6 +45,7 @@ export interface PlatformStatistics {
   placedOrders: number;
   totalFoodOrders: number;
   totalP2POrders: number;
+  totalDeliveries: number;
   averageFoodTicketPrice: number;
   averageCourierFare: number;
 }
