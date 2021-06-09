099eca7ba2d914d277deba846a2fe03abcb0b660
Pedro Andrade <pdandradeb@gmail.com>
2021-06-09 15:46:58 -0300

    Reorganising platform params

--- platform/params.ts
+++ platform/params.ts
@@ -1,13 +1,12 @@
 export interface PlatformParams {
-  maxDistanceToOrigin: number; // in meters
-  maxDistance: number; // in meters
-  averageCookingTime: number; // in seconds
-  averageWaitingTime: number; // in seconds
-  maxTimeOfArrivalFactor: number; // multipler used to calculate maxTimeOfArrival
-  bicyclingDurationFactor: number; // multipler used to calculate duration for byciling
-  matchingTimeBeforeReady: number; // amount of time (in seconds) before expected readiness to start matching;
   matching: {
+    maxDistance: number; // in meters
+    maxDistanceToOrigin: number; // in meters
     locationFreshnessThreshold: number; // 10 minutes
+    timeBeforeReady: number; // amount of time (in seconds) before expected readiness to start matching;
+    maxTimeOfArrivalFactor: number; // multiplier used to calculate maxTimeOfArrival
+    bicyclingDurationFactor: number; // multipler used to calculate duration for byciling
+    averageWaitingTime: number; // in seconds
   };
   courier: {
     approveAfterVerified: boolean;
@@ -15,6 +14,7 @@ export interface PlatformParams {
   business: {
     approveAfterVerified: boolean;
     keepAliveThreshold: number; // number of seconds to start refusing orders
+    averageCookingTime: number; // in seconds
   };
   maintenance: {
     backupsEnabled: boolean;
