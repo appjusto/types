578f0880dcb56c22b51a0799a76f05083a9f8f60
Pedro Andrade <pdandradeb@gmail.com>
2021-12-07 11:03:41 -0300

    Adding location to OrderMeta

--- order/meta.ts
+++ order/meta.ts
@@ -1,8 +1,10 @@
 import { IncomingHttpHeaders } from 'http';
+import { LatLng } from '..';
 
 export interface OrderMeta {
   headers: IncomingHttpHeaders;
   ip: string;
+  location: LatLng | null;
   app: {
     version: string;
   };
