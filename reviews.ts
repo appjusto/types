716dd9f61d3d3a78e9feff7b5ec369992966ff25
Daniel Lima <danielfbl@gmail.com>
2022-01-28 16:25:37 -0300

    replacing Review interface with OrderConsumerReview

--- reviews.ts
+++ reviews.ts
@@ -2,9 +2,23 @@ import firebase from 'firebase';
 
 export type ReviewType = 'positive' | 'negative';
 
-export interface Review {
-  type: ReviewType;
-  orderId?: string;
+export interface OrderConsumerReview {
+  orderId: string;
+  courier?: {
+    courierId: string;
+    rating: ReviewType;
+    tags?: string[];
+  };
+  business?: {
+    businessId: string;
+    rating: ReviewType;
+    tags?: string[];
+  };
+  platform?: {
+    rating: ReviewType;
+    tags?: string[];
+  };
+  nps?: 1 | 2 | 3 | 4 | 5;
   comment?: string;
   createdOn?: firebase.firestore.FieldValue;
 }
