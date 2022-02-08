152605d78840c56ca80e749838ca0fc235c0ec1f
Daniel Lima <danielfbl@gmail.com>
2022-02-08 13:48:45 -0300

    minor change (forgot to commit this before)

--- reviews.ts
+++ reviews.ts
@@ -21,7 +21,7 @@ export interface OrderConsumerReview {
     id: string | null;
     rating: ReviewType;
     tags?: ReviewTag[];
-  };
+  } | null;
   platform?: {
     rating: ReviewType;
     tags?: ReviewTag[];
