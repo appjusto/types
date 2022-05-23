57b6bdb34b22ca35f8e8c0f793701ce6d652f4fa
Pedro Andrade <pdandradeb@gmail.com>
2022-05-23 17:21:22 -0300

    Adding new fields for fraud prevention

--- order/fraud.ts
+++ order/fraud.ts
@@ -1,5 +1,7 @@
 export interface OrderFraudPreventionFlags {
   consumerHasConfirmedPhoneNumber?: boolean;
+  consumerHasPlacedTooManyOrdersRecently?: boolean;
+  consumerHasSuspectInvoices?: boolean;
   newUser?: boolean;
   highTicketPrice?: boolean;
   flaggedLocationsNearby?: boolean;
