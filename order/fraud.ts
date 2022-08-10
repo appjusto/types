9c166de63c84893e48afb831dee040f825ebf1ab
Pedro Andrade <pdandradeb@gmail.com>
2022-08-10 16:53:22 -0300

    Adding consumerConsideredSafe to OrderFraudPreventionFlags

--- order/fraud.ts
+++ order/fraud.ts
@@ -2,6 +2,7 @@ export interface OrderFraudPreventionFlags {
   consumerHasConfirmedPhoneNumber?: boolean;
   consumerHasPlacedTooManyOrdersRecently?: boolean;
   consumerHasSuspectInvoices?: boolean;
+  consumerConsideredSafe?: boolean;
   newUser?: boolean;
   highTicketPrice?: boolean;
   flaggedLocationsNearby?: boolean;
