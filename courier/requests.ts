f7c33c03d5448f033f5482afd5f1459cc39dc3a8
Pedro Andrade <pdandradeb@gmail.com>
2022-08-19 12:06:20 -0300

    Adding processing to CourierOrderRequest; Adding processing to Order.tip

--- courier/requests.ts
+++ courier/requests.ts
@@ -1,12 +1,18 @@
 import { FieldValue } from 'firebase/firestore';
 import { OrderType } from '../order';
 import { LatLng } from '../platform';
+import { Fee } from '../platform/fees';
 
 export interface CourierOrderRequest {
   type: OrderType;
   situation: 'pending' | 'accepted' | 'viewed' | 'rejected' | 'expired';
   orderId: string;
   fee: number;
+  processing: {
+    fee: Fee;
+    value: number;
+  };
+  /** @deprecated */
   processingFee: number;
   origin: LatLng;
   distanceToOrigin: number;
