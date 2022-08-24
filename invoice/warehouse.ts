1ab803bf312ff0c94c38751df8fff55d378cc657
Pedro Andrade <pdandradeb@gmail.com>
2022-08-24 10:15:31 -0300

    Adding fare to invoice warehouse

--- invoice/warehouse.ts
+++ invoice/warehouse.ts
@@ -1,12 +1,14 @@
 import { FieldValue } from 'firebase/firestore';
 import { InvoiceType } from '.';
 import { AccountType } from '..';
+import { FareDetails } from '../order/fare';
 import { IuguInvoiceStatus } from '../payment/iugu';
 
 export interface InvoiceWarehouse {
   invoiceType: InvoiceType;
   orderId: string;
   value: number;
+  fare: FareDetails | null;
   commission: number | null;
   accountType: AccountType;
   accountId: string | null;
