e8b63f81b0dbe31a795a0e64cbe5d8e0e3992021
Pedro Andrade <pdandradeb@gmail.com>
2022-10-18 11:03:02 -0300

    Adding platform.access.currentVersions

--- platform/access.ts
+++ platform/access.ts
@@ -5,6 +5,12 @@ export interface PlatformAccess {
     header?: string | null;
     body?: string[] | null;
   };
+  currentVersions: {
+    consumer: string | null;
+    courier: string | null;
+    businessWeb: string | null;
+    businessApp: string | null;
+  };
   minVersions: {
     consumer: string | null;
     courier: string | null;
