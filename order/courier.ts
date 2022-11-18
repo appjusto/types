3f0a9f00c8ed51bb63273607176c34df6b730059
Renan Costa <renancostam@gmail.com>
2022-11-18 15:02:56 -0300

    OrderCourier adjustments to include quotation and external link

--- order/courier.ts
+++ order/courier.ts
@@ -2,6 +2,48 @@ import { CourierMode, CourierStatistics } from '../courier';
 import { Timestamp } from '../external/firebase';
 import { LatLng } from '../platform';
 
+interface PriceBreakdown {
+  base?: string;
+  extraMileage?: string;
+  surcharge?: string;
+  coupon?: string;
+  specialRequests?: string;
+  priorityFee?: string;
+  priorityFeeVat?: string;
+  specialVehicle?: string;
+  minimumSurcharge?: string;
+  discountCap?: string;
+  insurance?: string;
+  multiStopSurcharge?: string;
+  surchargeDiscount?: string;
+  vat?: string;
+  customerSupportDiscretionary?: string;
+  totalBeforeOptimization?: string;
+  totalExcludePriorityFee?: string;
+  total: string;
+  currency: string;
+}
+
+interface Stop {
+  id?: string;
+  coordinates: {
+    lat: string;
+    lng: string;
+  };
+  address: string;
+}
+
+export interface OrderQuotation {
+  id: string;
+  scheduleAt: Date;
+  serviceType: string;
+  specialRequests: string[];
+  expiresAt: Date;
+  priceBreakdown: PriceBreakdown;
+  isRouteOptimized: boolean;
+  stops: Stop[];
+}
+
 export interface OrderCourier {
   id?: string;
   name?: string;
@@ -13,5 +55,7 @@ export interface OrderCourier {
   mode: CourierMode;
   statistics?: CourierStatistics;
   outsourcedOrderId?: string | null;
+  quotation?: OrderQuotation | null;
+  externalLink?: string | null;
   updatedOn?: Timestamp;
 }
