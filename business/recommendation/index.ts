c13f2137e5acb10a35da15d31933e6d9bc9f3ecb
Daniel Lima <danielfbl@gmail.com>
2022-11-30 12:34:41 -0300

    changing BusinessRecommendation interface

--- business/recommendation/index.ts
+++ business/recommendation/index.ts
@@ -1,9 +1,11 @@
 import { Timestamp } from '../../external/firebase';
 import { Place } from '../../order/place';
+import { Flavor } from '../../platform';
 
 export interface BusinessRecommendation {
   recommendedBusiness: Place;
-  consumerId: string | null;
+  flavor: Flavor;
+  userId: string | null;
   instagram: string | null;
   phone: string | null;
   owner: string | null;
