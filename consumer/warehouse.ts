abae0422d4311c8e4a1e0f57eb7b19edd94cd97a
Pedro Andrade <pdandradeb@gmail.com>
2023-01-25 14:39:19 -0300

    Replacing array for booleans on ConsumerWarehouse

--- consumer/warehouse.ts
+++ consumer/warehouse.ts
@@ -1,13 +1,14 @@
 import { ProfileSituation, ProfileTimestamps } from '..';
 import { GeoPoint, Timestamp } from '../external/firebase';
-import { NotificationPreferences } from '../profile/notifications';
 
 export type ConsumerWarehouse = {
   name: string | null;
   email: string | null;
   situation: ProfileSituation;
   notificationToken: string | null;
-  notificationPreferences: NotificationPreferences | null;
+  acceptsGeneralNotification: boolean;
+  acceptsMarketingNotification: boolean;
+  acceptsStatusNotification: boolean;
   appVersion: string | null;
   appInstallationId: string | null;
   platform: string | null;
