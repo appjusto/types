b3e1dc0b71c1a00cf9d1ab2caf889ccd21096355
Pedro Andrade <pdandradeb@gmail.com>
2023-02-16 09:30:30 -0300

    Updating task payloads

--- external/lalamove/payloads.ts
+++ external/lalamove/payloads.ts
@@ -1,4 +1,23 @@
-export interface LalamoveTaskPayload {
-  action: 'create-quotation' | 'create-order' | 'fetch-courier-info';
+export interface LalamoveBaseTaskPayload {
   orderId: string;
 }
+
+export interface LalamoveCreateQuotationTaskPayload
+  extends LalamoveBaseTaskPayload {
+  action: 'create-quotation';
+  createOrder: boolean;
+}
+
+export interface LalamoveCreateOrderTaskPayload
+  extends LalamoveBaseTaskPayload {
+  action: 'create-order';
+}
+export interface LalamoveFetchCourierInfoTaskPayload
+  extends LalamoveBaseTaskPayload {
+  action: 'fetch-courier-info';
+}
+
+export type LalamoveTaskPayload =
+  | LalamoveCreateQuotationTaskPayload
+  | LalamoveCreateOrderTaskPayload
+  | LalamoveFetchCourierInfoTaskPayload;
