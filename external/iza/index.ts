10223d6c7414946d04c150f3c5281d6549ec2330
Pedro Andrade <pdandradeb@gmail.com>
2023-02-17 10:13:26 -0300

    Updating Iza documents

--- external/iza/index.ts
+++ external/iza/index.ts
@@ -2,8 +2,19 @@ import { Timestamp } from '../firebase';
 import { IzaApiPerson } from './payloads';
 
 export interface IzaPerson {
+  doc: string;
+  birthday: string;
+  name: string;
   courierId: string;
   person: IzaApiPerson;
+  createdAt: Timestamp;
+}
+
+export interface IzaContract {
+  doc: string;
+  courierId: string;
+  contractId: string;
+  createdAt: Timestamp;
 }
 
 export type IzaPeriodStatus = 'started' | 'finished' | 'canceled';
@@ -11,6 +22,7 @@ export type IzaPeriodStatus = 'started' | 'finished' | 'canceled';
 export interface IzaPeriod {
   status: IzaPeriodStatus;
   orderId: string;
+  doc: string;
   courierId: string;
   externalId: string;
   startedAt: Timestamp;
