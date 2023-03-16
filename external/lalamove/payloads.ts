f7b4e46095ab240b1389f94d5993219f4d19fb9c
Pedro Andrade <pdandradeb@gmail.com>
2023-03-16 15:22:07 -0300

    Adding payload

--- external/lalamove/payloads.ts
+++ external/lalamove/payloads.ts
@@ -32,4 +32,5 @@ export type LalamoveTaskPayload =
   | LalamoveCreateQuotationTaskPayload
   | LalamoveCreateOrderTaskPayload
   | LalamoveFetchCourierInfoTaskPayload
-  | LalamoveAddPriorityFeePayload;
+  | LalamoveAddPriorityFeePayload
+  | LalamoveHandleHookPayload;
