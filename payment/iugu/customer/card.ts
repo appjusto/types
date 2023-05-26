129a23365e61669986e14efa6cbf0269d8294719
Pedro Andrade <pdandradeb@gmail.com>
2023-05-26 19:21:28 -0300

    Adding info to IuguCard

--- payment/iugu/customer/card.ts
+++ payment/iugu/customer/card.ts
@@ -1,5 +1,14 @@
+import { Timestamp } from '../../../external/firebase';
 import { BasePayload } from '../../../payloads';
 
+export interface IuguCard {
+  accountId: string;
+  accountType: 'consumer';
+  cardTokenId: string | null;
+  cardHash: string;
+  createdAt: Timestamp;
+  updatedAt?: Timestamp;
+}
 export interface SaveIuguCard extends BasePayload {
   cardTokenId: string;
   cardHash: string;
@@ -7,10 +16,6 @@ export interface SaveIuguCard extends BasePayload {
 export interface SaveIuguCardResponse extends BasePayload {
   cardTokenId: string;
 }
-
-export interface IuguCard {
-  accountId: string;
-  accountType: 'consumer';
-  cardTokenId: string;
-  cardHash: string;
+export interface DeleteIuguCard extends BasePayload {
+  id: string;
 }
