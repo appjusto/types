e692afea9d3207efbabe74ae0481f76328dd4eef
Pedro Andrade <pdandradeb@gmail.com>
2023-06-02 13:06:08 -0300

    Adding data to VRCard

--- payment/vr/card/index.ts
+++ payment/vr/card/index.ts
@@ -4,5 +4,13 @@ export interface VRCard extends Card {
   processor: 'vr';
   token: {
     id: string;
+    data: {
+      brand: 'vr';
+      display_number: string; // XXXX-XXXX-XXXX-1111
+      last_digits: string;
+      holder_name: string;
+      month: number;
+      year: number;
+    };
   };
 }
