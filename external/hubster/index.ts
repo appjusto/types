bbd09adf6246274ae053b90f8fa8545610def3c3
Renan Costa <renancostam@gmail.com>
2023-06-13 14:26:01 -0300

    Adding menu to HubsterStore

--- external/hubster/index.ts
+++ external/hubster/index.ts
@@ -4,4 +4,8 @@ export interface HubsterStore {
   businessId: string;
   storeId: string;
   status: HubsterStoreStatus;
+  menu?: {
+    source: 'appjusto' | 'hubster';
+    sync: boolean;
+  };
 }
