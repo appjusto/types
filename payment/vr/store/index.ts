6624b2aa3aa98e61c17762b01b4653da0ae99edc
Pedro Andrade <pdandradeb@gmail.com>
2023-06-15 15:53:30 -0300

    Updating VRStore

--- payment/vr/store/index.ts
+++ payment/vr/store/index.ts
@@ -1,8 +1,12 @@
+import { Timestamp } from '../../../external/firebase';
 import { VRPayableWith } from '../card';
 
+export type VRStoreStatus = 'available' | 'unavailable';
 export interface VRStore {
   businessId: string;
   affiliateId: number;
   paymentMethods: VRPayableWith[];
-  status: 'available' | 'unavailable';
+  status: VRStoreStatus;
+  createdAt: Timestamp;
+  updatedAt: Timestamp | null;
 }
