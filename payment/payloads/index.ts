9c74ce927e6be2674e275bdbdce7d3c3762d9f9a
Pedro Andrade <pdandradeb@gmail.com>
2023-06-21 11:50:44 -0300

    Adding from to update payment refund

--- payment/payloads/index.ts
+++ payment/payloads/index.ts
@@ -4,6 +4,7 @@ export interface RefundPaymentPayload extends BasePayload {
   action: 'refund';
   paymentId: string;
   value?: number;
+  from?: 'business' | 'platform';
 }
 
 export type UpdatePaymentPayload = RefundPaymentPayload;
