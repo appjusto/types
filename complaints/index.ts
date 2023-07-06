138ca122f591f1202102d083b01c7a7b29002172
Pedro Andrade <pdandradeb@gmail.com>
2023-07-06 15:28:02 -0300

    Adding ContactBy

--- complaints/index.ts
+++ complaints/index.ts
@@ -1,15 +1,22 @@
 import { Flavor, OrderStaff } from '..';
 import { Timestamp } from '../external/firebase';
 
+export type ComplaintStatus =
+  | 'pending'
+  | 'investigating'
+  | 'upheld'
+  | 'inconclusive';
+export type ContactBy = 'whatsapp' | 'e-mail';
 export interface Complaint {
   against: string;
   place: string;
   date: string;
   description: string;
   flavor: Flavor;
+  contactBy: 'whatsapp' | 'e-mail';
   orderId?: string | null;
   staff?: OrderStaff | null;
-  status: 'pending' | 'investigating' | 'upheld' | 'inconclusive';
+  status: ComplaintStatus;
   createdBy: string;
   createdAt: Timestamp;
   updatedAt?: Timestamp;
