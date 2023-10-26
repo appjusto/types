64add28f49260fbe8a9e8d283ed9dbb96115c971
Pedro Andrade <pdandradeb@gmail.com>
2023-10-26 08:39:03 -0300

    Updating courier costs

--- courier/costs.ts
+++ courier/costs.ts
@@ -1,27 +1,44 @@
 import { Timestamp } from '../external/firebase';
 
 export interface CourierCosts {
+  // deliveires
   ordersPerDay: number;
   hoursPerDay: number;
   daysPerWeek: number;
+  revenuePerOrder: number;
+  // distance
   distanceFromHome: number;
   distanceToOrigin: number;
   distanceToDestination: number;
-  foodPerDay: number;
-  carrierPerMonth: number;
-  insurancePerMonth: number;
+  // vehicle
   vehicle: 'motorcycle' | 'bike' | 'electric';
   kmWithLiter: number;
   gasPrice: number;
+  vehicleInsurancePerYear: number;
   taxesPerYear: number;
   maintenancePerYear: number;
-  vehicleInsurancePerYear: number;
-  numberOfWithdrawsPerMonth: number;
+  // costs
   withdrawCosts: number;
-  revenuePerOrder: number;
-  mei: boolean;
+  carrierPerMonth: number;
+  foodPerDay: number;
+  insurancePerMonth: number;
   otherCostsPerDay: number;
   otherCostsPerYear: number;
-  email: string;
+  // meta
   updatedAt: Timestamp;
+  // processing
+  processing?: {
+    dailyCosts: number;
+    monthlyCosts: number;
+    yearlyCosts: number;
+    dailyCostsPerOrder: number;
+    monthlyCostsPerOrder: number;
+    yearlyCostsPerOrder: number;
+    costsPerOrder: number;
+    orderGasCost: number;
+    orderRevenue: number;
+    hourRevenue: number;
+    minimumWageComparison: number;
+    livingWageComparison: number;
+  };
 }
