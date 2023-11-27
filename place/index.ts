9f5d6e1c62df2cb7eb0f9a4c0d08d0912ca89bd8
Pedro Andrade <pdandradeb@gmail.com>
2023-11-27 10:44:59 -0300

    Allowing additionalInfo to be null

--- place/index.ts
+++ place/index.ts
@@ -11,7 +11,7 @@ export interface Address {
 export interface Place {
   accountId?: string;
   address: Address;
-  additionalInfo?: string;
+  additionalInfo?: string | null;
   instructions?: string;
   name?: string;
   geohash?: string | null;
