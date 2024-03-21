5559c55a2de8201fe9b3394c831e78c3ad1898b2
Pedro Andrade <pdandradeb@gmail.com>
2024-03-21 15:59:26 -0300

    Adding averageCookingTime to BusinessAlgolia

--- business/algolia.ts
+++ business/algolia.ts
@@ -21,6 +21,7 @@ export interface BusinessAlgolia {
   enabled: boolean;
   status: BusinessStatus;
   opened: boolean;
+  averageCookingTime: string;
   name?: string;
   slug?: string;
   code?: string;
